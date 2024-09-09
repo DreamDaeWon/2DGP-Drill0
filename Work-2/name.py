6f2b15690b1440fe89e52f5dd9feb669845827bc
DreamDaeWon@NoteBookOmen <june25656@gmail.com>
2024-09-09 15:12:47 +0900

    24 09 09 15:12 Work-2 finsh

---
+++ Work-2/name.py
@@ -0,0 +1,69 @@
+import turtle
+turtle.goto(0,100) #ㅂ
+turtle.goto(0,50)
+turtle.goto(50,50)
+turtle.goto(50,100)
+turtle.goto(50,0)
+turtle.goto(0,0)
+
+turtle.penup() #ㅏ
+
+turtle.goto(75,100)
+turtle.pendown()
+turtle.goto(75,0)
+turtle.goto(75,50)
+turtle.goto(100,50)
+
+
+turtle.penup() # ㄱ
+turtle.goto(25,-50)
+turtle.pendown()
+turtle.goto(75,-50)
+turtle.goto(75,-100)
+
+turtle.penup() # ㄷ
+turtle.goto(125,0)
+turtle.pendown()
+turtle.goto(125,50)
+turtle.goto(125,-50)
+turtle.goto(125,50)
+turtle.goto(175,50)
+turtle.goto(125,50)
+turtle.goto(125,-50)
+turtle.goto(175,-50)
+
+turtle.penup() # ㅐ
+turtle.goto(200,0)
+turtle.pendown()
+turtle.goto(200,50)
+turtle.goto(200,-50)
+turtle.goto(200,0)
+turtle.goto(225,0)
+turtle.goto(225,50)
+turtle.goto(225,-50)
+
+turtle.penup() # o
+turtle.goto(280,50)
+turtle.pendown()
+turtle.circle(30)
+
+turtle.penup() # ㅜ
+turtle.goto(280,25)
+turtle.pendown()
+turtle.goto(250,25)
+turtle.goto(310,25)
+turtle.goto(275,25)
+turtle.goto(275,-25)
+
+turtle.penup() # ㅓ
+turtle.goto(285,0)
+turtle.pendown()
+turtle.goto(320,0)
+turtle.goto(320,50)
+turtle.goto(320,-50)
+
+turtle.penup() # ㄴ
+turtle.goto(270,-50)
+turtle.pendown()
+turtle.goto(270,-80)
+turtle.goto(330,-80)
